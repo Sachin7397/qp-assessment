349ad39d641afa0f6e03014def08f03e71ac29f2
Sachin7397 <sachinskamble0910@gmail.com>
2025-02-23 00:54:45 +0530

    server files

---
+++ dist/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
+const groceryRoutes_1 = __importDefault(require("./routes/groceryRoutes"));
+const orderRoutes_1 = __importDefault(require("./routes/orderRoutes"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)());
+app.use("/auth", authRoutes_1.default);
+app.use("/grocery", groceryRoutes_1.default);
+app.use("/orders", orderRoutes_1.default);
+exports.default = app;
